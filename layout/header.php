ff62ba19fcb20273f34e367129afcf3473e9e248
Drew Wetherington <dwetherington@cibotechnologies.com>
2018-01-18 01:14:45 -0600

    changed header and drop shadow

--- layout/header.php
+++ layout/header.php
@@ -3,25 +3,25 @@
 	<div id="logo_text">
 	  <!-- class="logo_colour", allows you to change the colour of the text -->
 	  <?php
-	  
+
 	  $title = explode(" ", $config['site_title']);
-	  
+
 	  ?>
-	  <h1><a href="index.php"><?php
-	  
+	  <h1 class="contrast"><a href="index.php"><?php
+
 	  if (count($title) > 1) {
 		foreach ($title as $word) {
 			if ($word !== $title[count($title) - 1]) echo $word .' ';
 		}
 	  } else echo $config['site_title'];
 	  ?><span class="logo_colour"><?php
-	  
+
 	  if (count($title) > 1) {
 		echo ($title[count($title) - 1]);
 	  }
-	  
+
 	  ?></span></a></h1>
-	  <h2><?php echo $config['site_title_context']; ?></h2>
+	  <h2 class="contrast"><?php echo $config['site_title_context']; ?></h2>
 	</div>
   </div>
   <nav>
